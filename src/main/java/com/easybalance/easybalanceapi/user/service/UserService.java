b09501b36a74ae01856bd87f142f1c48bdcd4064
Carlos Takeshi Sato <carlostakeshi100@gmail.com>
2024-08-17 22:43:33 -0300

    feat: add exceptions for user

--- src/main/java/com/easybalance/easybalanceapi/user/service/UserService.java
+++ src/main/java/com/easybalance/easybalanceapi/user/service/UserService.java
@@ -2,7 +2,9 @@ package com.easybalance.easybalanceapi.user.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.easybalance.easybalanceapi.user.api.request.CreateUserRequest;
 import com.easybalance.easybalanceapi.user.model.entity.User;
@@ -17,6 +19,10 @@ public class UserService {
     private final UserRepository repository;
 
     public User save(CreateUserRequest request) {
+        if (repository.findByEmail(request.getEmail()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "User already exists");
+        }
+
         return repository.save(
             User.builder()
                 .name(request.getName())
@@ -30,20 +36,15 @@ public class UserService {
     }
 
     public User getById(Long id) {
-        return repository.findById(id).orElse(null);
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
     public User getByEmail(String email) {
-        return repository.findByEmail(email).orElse(null);
+        return repository.findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
     public void deleteById(Long id) {
         repository.deleteById(id);
-    }
-
-    public void deleteByEmail(String email) {
-        User user = getByEmail(email);
-        repository.delete(user);
     }
 
 }
